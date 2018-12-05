226fd08a1d728253b4f4709cecfe46dbdc4e90c8
vkov93 <vkov93@gmail.com>
2018-12-05 22:19:44 +0300

    Add methods and fixed instructions This version need upgrade, split line doesn't work well.

--- src/FindStr.java
+++ src/FindStr.java
@@ -4,10 +4,37 @@ import java.util.Arrays;
 import java.util.ArrayList;
 
 public class FindStr {
-    private static int BUFFER_SIZE = 128;
 
-    private static String convert(int line, int pos){
-        return "[" + line + "," + pos + "]";
+    private static int BUFFER_SIZE = 128;
+    private static int prevline = -1;
+    private static int index = 0;
+    private static ArrayList<String> result = new ArrayList<String>();
+
+    private static String convert(int line, int pos){ return "[" + line + "," + pos + "]"; }
+    //method for find and write substrings indexes
+
+    private static int findSubStrIndexes(String[] splitStrings, String substr, int linecount){
+        int positionsv;
+        //prevline = linecount;
+        for(String str : splitStrings) {
+            linecount++;
+            if(prevline == linecount) index++;
+            else index = 0;
+
+            positionsv = 0;  //find all substrings in line
+            while (positionsv != -1) {
+                positionsv = str.indexOf(substr, positionsv);
+                if (positionsv != -1) {
+                    result.add(convert(linecount, positionsv + index * BUFFER_SIZE));
+                    positionsv++;
+                }
+                else break;
+            }
+            prevline = linecount;
+            if(splitStrings.length > 1) linecount++;
+
+        }
+        return linecount;
     }
 
     public static void main(String[] args) {
@@ -23,42 +50,30 @@ public class FindStr {
         int positionsv;
         int linecount = 0;
         byte[] buffer = new byte[BUFFER_SIZE];
-        byte[] check = new byte[BUFFER_SIZE];
-
-        ArrayList<String> result = new ArrayList<String>();
 
         String line;
         String substr = args[1];
-        String[] subStrings;
-        //String READ_TEXT = "";  //instrument for reading text
+        String[] splitStrings;
+        String READ_TEXT = "";
 
         try(FileInputStream fr = new FileInputStream(args[0])){
-            for(;;) {
-
-                fr.read(buffer, 0 , BUFFER_SIZE);
-                if(Arrays.equals(buffer, check)) break; //when read(buffer) is empty (EOF) - break all
+            while(fr.read(buffer, 0 , BUFFER_SIZE) != -1) {
                 line = new String(buffer);
-                subStrings = line.split("\n"); //split line
-                for(int i = 0; i < subStrings.length; i++) {
-                    positionsv = 0;  //find all substrings in line
-                    while (positionsv != -1) {
-                        positionsv = subStrings[i].indexOf(substr, positionsv);
-                        if (positionsv != -1) result.add(convert(linecount, positionsv++));
-                        else break;
-                    }
-                    linecount++;
-                    //increase line index
-                }
-                //READ_TEXT += line + '\n';
-                buffer = new byte[BUFFER_SIZE]; //recovery buffer
-                if(subStrings.length == 1) linecount--;
-            }
+                splitStrings = line.split("\n");
 
-            //System.out.println(READ_TEXT);
+                linecount = FindStr.findSubStrIndexes(splitStrings, substr,linecount - 1);
+
+                READ_TEXT += line;
+                buffer = new byte[BUFFER_SIZE]; //recovery buffer for next line
+
+            }
+            System.out.println(READ_TEXT);
             System.out.print(result); //print ArrayList with indexes
         }catch (IOException e){
             e.printStackTrace();
         }
+        System.out.println();
+
 
     } //main
 } //class
