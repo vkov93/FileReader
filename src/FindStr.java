c8f2ba291fab8d3e82d008eb53f053a1efab8f88
vkov93 <vkov93@gmail.com>
2018-12-06 21:07:53 +0300

    Return Entry class and fix some warnings

--- src/FindStr.java
+++ src/FindStr.java
@@ -1,41 +1,66 @@
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.ArrayList;
 
 public class FindStr {
+    private static int count;
+    private static final int BUFFER_SIZE = 8192;
+    private static String[] splitStrings;
+    private static ArrayList<Entry> result = new ArrayList<>();
 
-    private static int BUFFER_SIZE = 128;
-    private static int prevline = -1;
-    private static int index = 0;
-    private static ArrayList<String> result = new ArrayList<String>();
+    //simple class for save entry point
+    private static class Entry{
+        private int line;
+        private int pos;
 
-    private static String convert(int line, int pos){ return "[" + line + "," + pos + "]"; }
+        private Entry(int line, int pos){
+            this.line = line;
+            this.pos = pos;
+        }
+
+        int getLine(){
+            return line;
+        }
+        int getPos(){
+            return  pos;
+        }
+        @Override
+        public String toString() {
+            return "[" + line + "," + pos + "]";
+        }
+    }
+
     //method for find and write substrings indexes
+    private static Entry findSubStrings(Entry current, String substring){
+        int line = current.getLine();
+        int pos = current.getPos();
 
-    private static int findSubStrIndexes(String[] splitStrings, String substr, int linecount){
-        int positionsv;
-        //prevline = linecount;
-        for(String str : splitStrings) {
-            linecount++;
-            if(prevline == linecount) index++;
-            else index = 0;
-
-            positionsv = 0;  //find all substrings in line
-            while (positionsv != -1) {
-                positionsv = str.indexOf(substr, positionsv);
-                if (positionsv != -1) {
-                    result.add(convert(linecount, positionsv + index * BUFFER_SIZE));
-                    positionsv++;
+        for(String str : splitStrings){
+            //find all substring indexes
+            while (pos != -1) {
+                pos = str.indexOf(substring, pos);
+                if (pos != -1) {
+                    result.add(new Entry(line, pos + count * BUFFER_SIZE));
+                    pos++;
                 }
-                else break;
+                else {
+                    pos = 0;
+                    break;
+                }
             }
-            prevline = linecount;
-            if(splitStrings.length > 1) linecount++;
+            if(splitStrings.length != 1) {
+                pos = 0;
+                line++;
+                count = 0;
+            }
+            else count++;
 
         }
-        return linecount;
+
+        current = new Entry(line, pos);
+        return current;
     }
+
 
     public static void main(String[] args) {
         if (args.length != 2) {
@@ -43,38 +68,31 @@ public class FindStr {
                     "\nИспользование: file_name substr;" +
                     "\nПример: examplefile.txt bananas - " +
                     "результат - массив индексов всех вхождений bananas в файле" +
-                    "\n в формате \"Вхождение на строке \" + line + \" на позиции \" + pos");
+                    "\n в формате \"[line\" , \"pos]");
             return;
         }
         //init (maybe drop this into initialisation block???)
-        int positionsv;
-        int linecount = 0;
         byte[] buffer = new byte[BUFFER_SIZE];
 
-        String line;
-        String substr = args[1];
-        String[] splitStrings;
-        String READ_TEXT = "";
+        String symbols;
+        String substring = args[1];
+
+        Entry current = new Entry(0, 0);
 
         try(FileInputStream fr = new FileInputStream(args[0])){
             while(fr.read(buffer, 0 , BUFFER_SIZE) != -1) {
-                line = new String(buffer);
-                splitStrings = line.split("\n");
+                symbols = new String(buffer);
+                splitStrings = symbols.split("\n");
+                current = findSubStrings(current, substring);
 
-                linecount = FindStr.findSubStrIndexes(splitStrings, substr,linecount - 1);
+                buffer = new byte[BUFFER_SIZE]; //recovery buffer for next symbols
 
-                READ_TEXT += line;
-                buffer = new byte[BUFFER_SIZE]; //recovery buffer for next line
-
+                System.out.print(symbols); //check text
             }
-            System.out.println(READ_TEXT);
-            System.out.print(result); //print ArrayList with indexes
+            System.out.print("\n" + result);
         }catch (IOException e){
             e.printStackTrace();
         }
-        System.out.println();
-
-
     } //main
 } //class
 
